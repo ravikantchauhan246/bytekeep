85620fb628cf7ba210c9b7fae9ff8f146c0f803e
Ravikant Chauhan <53376708+ravikantchauhan246@users.noreply.github.com>
2024-11-23 12:46:24 +0530

    add tailwindcss-animate and update layout styles

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,12 +1,13 @@
 import type { Metadata } from "next";
 import "./globals.css";
-import {Poor_Story, Poppins} from "next/font/google";
+import { Poppins } from "next/font/google";
+import React from "react";
 
 const poppins = Poppins({
-  subsets:['latin'],
-  weight: ['100','200','300','400','500','600','700','800','900'],
-  variable:'--font-poppins'
-})
+  subsets: ["latin"],
+  weight: ["100", "200", "300", "400", "500", "600", "700", "800", "900"],
+  variable: "--font-poppins",
+});
 
 export const metadata: Metadata = {
   title: "ByteKeep",
@@ -20,9 +21,7 @@ export default function RootLayout({
 }>) {
   return (
     <html lang="en">
-      <body
-        className={`${poppins.variable} font-poppins antialiased`}
-      >
+      <body className={`${poppins.variable} font-poppins antialiased`}>
         {children}
       </body>
     </html>
