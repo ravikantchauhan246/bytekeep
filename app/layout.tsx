86114441bdabb03e00c1f624dfe2d126568e8fdf
Ravikant Chauhan <53376708+ravikantchauhan246@users.noreply.github.com>
2024-11-23 11:43:37 +0530

    initialize Next.js project with TypeScript, Tailwind CSS, and ESLint configuration

---
+++ app/layout.tsx
@@ -0,0 +1,30 @@
+import type { Metadata } from "next";
+import "./globals.css";
+import {Poor_Story, Poppins} from "next/font/google";
+
+const poppins = Poppins({
+  subsets:['latin'],
+  weight: ['100','200','300','400','500','600','700','800','900'],
+  variable:'--font-poppins'
+})
+
+export const metadata: Metadata = {
+  title: "ByteKeep",
+  description: "The vault to keep your bytes safe.",
+};
+
+export default function RootLayout({
+  children,
+}: Readonly<{
+  children: React.ReactNode;
+}>) {
+  return (
+    <html lang="en">
+      <body
+        className={`${poppins.variable} font-poppins antialiased`}
+      >
+        {children}
+      </body>
+    </html>
+  );
+}
